47a6614b57e2726a8715f8205ef9c2b3f57de22e
Warren <skyblade927@hotmail.com>
2016-08-30 22:43:19 +0800

    Update

---
+++ src/app/Model/PokemonInfo.ts
@@ -0,0 +1,12 @@
+export class PokemonInfo{
+    No:number;
+    Name:string;
+    Ability: string[];//特性
+    Gender: string;//性別
+    Color: string;
+    Height: number;
+    Weight: number;
+    Egg: string;
+    EggCount: number;
+    TotalExp: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,49 +19,15 @@ export class AppComponent {
   @Input() SpDef:number;
   title = 'app works!';
   constructor(){
-    this.HP=10;
-    this.Atk=20;
-    this.Def=30;
-    this.Spd=40;
-    this.SpDef=50;
-    this.SpAtk=60;
+    this.Rnd();
   }
-
-  c1(){
-      this.HP+=10;
-  }
-  c2(){
-      this.Atk+=10;
-  }
-  c3(){
-      this.Def+=10;
-  }
-  c4(){
-      this.Spd+=10;
-  }
-  c5(){
-      this.SpDef+=10;
-  }
-  c6(){
-      this.SpAtk+=10;
-  }
-  c7(){
-      this.HP-=10;
-  }
-  c8(){
-      this.Atk-=10;
-  }
-  c9(){
-      this.Def-=10;
-  }
-  c10(){
-      this.Spd-=10;
-  }
-  c11(){
-      this.SpDef-=10;
-  }
-  c12(){
-      this.SpAtk-=10;
+  Rnd(){
+    this.HP   = Math.floor(Math.random() * 80);
+    this.Atk  = Math.floor(Math.random() * 80);
+    this.Def  = Math.floor(Math.random() * 80);
+    this.Spd  = Math.floor(Math.random() * 80);
+    this.SpDef= Math.floor(Math.random() * 80);
+    this.SpAtk= Math.floor(Math.random() * 80);
   }
     
 }
